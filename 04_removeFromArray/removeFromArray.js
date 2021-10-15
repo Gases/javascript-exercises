ef7b80063b6ce46f6e7d86ed386034c11f9a387d
Alejandro <aariasmartijena@gmail.com>
2021-10-15 11:21:36 -0400

    Solved the Fundamentals part 4 JS exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {  
+    for (let i = 0; i < items.length; i++) {
+        if (array.indexOf(items[i]) === -1) {
+            continue;
+        } else {
+            array.splice(array.indexOf(items[i]),1);
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    let sum = 0;
 
+    if (typeof start !== "number" || typeof stop !== "number" || start < 0 || stop < 0) return "ERROR";
+
+    let orderedParams = Array.from(arguments).sort((a,b) => a - b);
+
+    for (let i = orderedParams[0]; i <= orderedParams[1]; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
